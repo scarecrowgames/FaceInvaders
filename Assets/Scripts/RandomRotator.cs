2127886462751043526e2bc0590ba92a8d9bdcbd
ScaryCrow <apthriver@gmail.com>
2016-03-22 23:53:23 -0400

    initial upload

---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotator : MonoBehaviour {
+
+    public float tumble;
+
+    private Vector3 rotSpeed;
+
+    void Start ()
+    {
+        rotSpeed = Random.insideUnitSphere * tumble;
+    }
+    void Update()
+    {
+        GetComponent<Transform>().Rotate(rotSpeed * Time.deltaTime);
+    }
+}
